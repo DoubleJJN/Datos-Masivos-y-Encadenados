31a83a3ebccb7c2f2599f2ef4c0d3038c3be797b
Marcos <marcal16@ucm.es>
2024-11-06 01:09:48 +0100

    Update db schema and more

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Destination, Accommodation, Activity, Itinerary, ItineraryActivity, Comment
+from .models import Destination, Accommodation#, Activity, Itinerary, ItineraryActivity, Comment
 
 admin.site.register(Destination)
 admin.site.register(Accommodation)
-admin.site.register(Activity)
-admin.site.register(Itinerary)
-admin.site.register(ItineraryActivity)
-admin.site.register(Comment)
+# admin.site.register(Activity)
+# admin.site.register(Itinerary)
+# admin.site.register(ItineraryActivity)
+# admin.site.register(Comment)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#Country, city or main destination
 class Destination(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
@@ -13,6 +14,7 @@ class Destination(models.Model):
     def __str__(self):
         return f"{self.name}, {self.city}, {self.country}"
 
+#Hotels, hostels, airbnb, etc.
 class Accommodation(models.Model):
     name = models.CharField(max_length=100)
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
@@ -25,49 +27,49 @@ class Accommodation(models.Model):
     def __str__(self):
         return f"{self.name} in {self.destination}"
 
-class Activity(models.Model):
-    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    schedule = models.CharField(max_length=100)
-    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(blank=True, null=True)
+# class Activity(models.Model):
+#     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     schedule = models.CharField(max_length=100)
+#     rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+#     image_url = models.URLField(blank=True, null=True)
 
-    def __str__(self):
-        return f"{self.name} in {self.destination}"
+#     def __str__(self):
+#         return f"{self.name} in {self.destination}"
 
-class Itinerary(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+# class Itinerary(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     start_date = models.DateField()
+#     end_date = models.DateField()
+#     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"Itinerary {self.name} by {self.user}"
+#     def __str__(self):
+#         return f"Itinerary {self.name} by {self.user}"
 
-class ItineraryActivity(models.Model):
-    itinerary = models.ForeignKey(Itinerary, on_delete=models.CASCADE)
-    activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    date = models.DateField()
-    time = models.TimeField()
+# class ItineraryActivity(models.Model):
+#     itinerary = models.ForeignKey(Itinerary, on_delete=models.CASCADE)
+#     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     time = models.TimeField()
 
-    def __str__(self):
-        return f"Activity {self.activity} in itinerary {self.itinerary}"
+#     def __str__(self):
+#         return f"Activity {self.activity} in itinerary {self.itinerary}"
 
 
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    destination = models.ForeignKey(Destination, on_delete=models.SET_NULL, null=True, blank=True)
-    accommodation = models.ForeignKey(Accommodation, on_delete=models.SET_NULL, null=True, blank=True)
-    activity = models.ForeignKey(Activity, on_delete=models.SET_NULL, null=True, blank=True)
-    comment = models.TextField()
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
+# class Comment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     destination = models.ForeignKey(Destination, on_delete=models.SET_NULL, null=True, blank=True)
+#     accommodation = models.ForeignKey(Accommodation, on_delete=models.SET_NULL, null=True, blank=True)
+#     activity = models.ForeignKey(Activity, on_delete=models.SET_NULL, null=True, blank=True)
+#     comment = models.TextField()
+#     rating = models.DecimalField(max_digits=3, decimal_places=2)
+#     date = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Comment by {self.user} - Rating: {self.rating}"
+#     def __str__(self):
+#         return f"Comment by {self.user} - Rating: {self.rating}"
 
 # class Booking(models.Model):
 #     user = models.ForeignKey(User, on_delete=models.CASCADE)
